1caedbfa791ff4f148e024b708f1edb1f739bfb2
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-09-02 22:14:32 +0200

    Add Product Rendering

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@
 
   const select = {
     templateOf: {
-      menuProduct: '#template-menu-product',
+      menuProduct: '#template-menu-product', /* selektor szablonu produktu */
     },
     containerOf: {
       menu: '#product-list',
@@ -59,7 +59,27 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* [DONE] generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      // console.log(generatedHTML);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+
     }
   }
 
