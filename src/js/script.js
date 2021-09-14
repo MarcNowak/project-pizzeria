17e04a6540fb082619d94627a465043dcda44b6c
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-09-14 11:18:35 +0200

    9.5 Updating totals after changing quantities - FINAL

--- src/js/script.js
+++ src/js/script.js
@@ -390,7 +390,10 @@
     announce() {
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
+
       thisWidget.element.dispatchEvent(event);
     }
   }
@@ -427,6 +430,11 @@
       thisCart.dom.toggleTrigger.addEventListener('click', function () {
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+
+      thisCart.dom.productList.addEventListener('updated', function () {
+        thisCart.update();
+      });
+      
     }
 
     add(menuProduct) {
